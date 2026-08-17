467870e0c7b5d90b7242725b7e934bb00af9af73
N1K0LA22 <108944930+N1K0LA22@users.noreply.github.com>
2026-08-17 19:12:38 -0300

    Create Média.c

---
+++ Média.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	
+	int a,b,c,d,e;
+	
+	a = 10;
+	b = 20;
+	c = 30;
+	d = (a+b+c)/3;
+	
+	
+	printf("A média de %d, %d e %d = %d", a,b,c,d);
+	
+return 0;
